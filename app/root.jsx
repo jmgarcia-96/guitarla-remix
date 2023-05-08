4372a2fd6325c385171ba2ebb77bf5ac019f2c97
jesgarlan <96166634+jmgarcia-96@users.noreply.github.com>
2023-05-08 10:16:37 +0200

    Sesión 11 - Proyecto inicial

---
+++ app/root.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <Document>
+      <h1>Hola Mundo</h1>
+    </Document>
+  );
+}
+
+function Document({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <title>GuitarLA - Remix</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
